10f75bf2928eb60da152ef17a85250ae9e5d848d
Nayeon Kim <nayeonkim93@gmail.com>
2025-06-02 22:55:22 -0700

    adding sliding animation to nav transition

--- navbar.js
+++ navbar.js
@@ -16,6 +16,7 @@ class NavBar extends HTMLElement {
                     </div>
                     <nav class="navbar-nav-wrapper">
                         <ul class="navbar-nav">
+                            ${isBento ? '<div class="nav-indicator"></div>' : ''}
                             <li><a href="/index.html#portfolio" class="nav-link">Work</a></li>
                             <li><a href="/creative.html" class="nav-link">Play</a></li>
                             <li><a href="/about.html" class="nav-link">About</a></li>
@@ -24,6 +25,36 @@ class NavBar extends HTMLElement {
                 </div>
             </header>
         `;
+
+        // Sliding indicator logic for bento style
+        if (isBento) {
+            const nav = this.querySelector('.navbar-nav');
+            const indicator = nav.querySelector('.nav-indicator');
+            const links = nav.querySelectorAll('.nav-link');
+
+            function moveIndicator(link) {
+                const rect = link.getBoundingClientRect();
+                const navRect = nav.getBoundingClientRect();
+                indicator.style.left = (link.offsetLeft) + 'px';
+                indicator.style.width = link.offsetWidth + 'px';
+                indicator.style.top = (link.offsetTop) + 'px';
+                indicator.style.height = link.offsetHeight + 'px';
+                indicator.style.opacity = 1;
+            }
+
+            function hideIndicator() {
+                indicator.style.opacity = 0;
+            }
+
+            links.forEach(link => {
+                link.addEventListener('mouseenter', () => moveIndicator(link));
+                link.addEventListener('focus', () => moveIndicator(link));
+                link.addEventListener('mouseleave', hideIndicator);
+                link.addEventListener('blur', hideIndicator);
+            });
+
+            hideIndicator();
+        }
     }
 }
 
